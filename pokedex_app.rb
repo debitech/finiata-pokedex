61f261f69b418f506dfa12047442e9fadba3df5f
MariaBraganca <mcpbbraganca@gmail.com>
2020-11-23 16:24:25 +0100

    pulled date from api and displayed it on index page

--- pokedex_app.rb
+++ pokedex_app.rb
@@ -1,8 +1,19 @@
 class PokedexApp < Sinatra::Application
   enable :sessions
 
+  require 'json'
+  require 'open-uri'
+
   get '/' do
-    display_page :index
+    # display_page :index
+
+    id = rand(1..10)
+
+    pokemon_url = "https://pokeapi.co/api/v2/pokemon/#{id}/"
+    pokemon_serialized = open(pokemon_url).read
+    pokemon = JSON.parse(pokemon_serialized)
+
+    erb :index, locals: {pokemon: pokemon}
   end
 
   def display_page(location, locals = {})
